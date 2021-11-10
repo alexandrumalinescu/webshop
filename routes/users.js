f9a4c0b8c6d042471cacee6327c6573ad9c59dad
Catalin Tudorache <catalin.tudorache87@gmail.com>
2021-11-10 09:25:19 +0200

    Initial project files

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const axios = require('axios').default;
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  // obtain the users from an API call
+  axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(function (response) {
+    console.log(response.data);
+
+    res.render('users', { title: 'Users', users: response.data });
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+});
+
+module.exports = router;
